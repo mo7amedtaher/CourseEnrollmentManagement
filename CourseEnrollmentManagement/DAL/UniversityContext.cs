a779372c3d498caabc97485841c2e411df9efb9f
Mohamed Taher <mo7amed_taher@hotmail.com>
2019-01-28 14:09:14 +0300

    eAdding Domain Models

---
+++ CourseEnrollmentManagement/DAL/UniversityContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using CourseEnrollmentManagement.Models;
+
+
+
+namespace CourseEnrollmentManagement.DAL
+{
+    public class UniversityContext : DbContext 
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        
+    }
+    
+}
